75c1823f95c7d755bb05608d8e43ebe07cb56bf4
devpraveen002 <dev.praveen002@gmail.com>
2024-12-18 20:02:00 +0530

    Deployed without error

--- Calendar.UI/Migrations/20241217200059_eventNullable.Designer.cs
+++ Calendar.UI/Migrations/20241217200059_eventNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Calendar.UI.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Calendar.UI.Migrations
 {
     [DbContext(typeof(CalendarDbContext))]
-    partial class CalendarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217200059_eventNullable")]
+    partial class eventNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
